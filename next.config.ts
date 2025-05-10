23f142300c873bbe7e52fadb10c17426e689886b
Utkarsh Tiwari <uktiwari023@gmail.com>
2025-05-10 11:54:29 +0530

    docker build fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+   output: 'standalone',
    images: {
       domains: ['cdn.rareblocks.xyz', 'unsplash.com','plus.unsplash.com','randomuser.me'], 
    },
